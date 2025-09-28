4228c8270c61c00aa227f6f09783d7da88d9b38a
Kingmouaad <kjhfu80@gmail.com>
2025-09-28 09:00:26 +0100

    second commit

--- dist/chbeb.js
+++ dist/chbeb.js
@@ -1,30 +1,23 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-function aynsfunction() {
-    return __awaiter(this, void 0, void 0, function* () {
-        let tafasir = [];
-        const quran = yield fetch("https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/ara-quranwarsh/1/1.json");
-        const quranresponse = yield quran.json();
-        // for (let i = 0; i < 5; i++) {
-        const tafsir = yield fetch(`https://cdn.jsdelivr.net/gh/spa5k/tafsir_api@main/tafsir/en-al-jalalayn/1/1.json`);
-        const tafsirresponse = yield tafsir.json();
-        tafasir.push(tafsirresponse);
-        // }
-        return [quranresponse, tafasir];
-    });
+async function aynsfunction() {
+  let tafasir = [];
+  const quran = await fetch(
+    "https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/ara-quranwarsh/1/1.json"
+  );
+  const quranresponse = await quran.json();
+  // for (let i = 0; i < 5; i++) {
+  const tafsir = await fetch(
+    `https://cdn.jsdelivr.net/gh/spa5k/tafsir_api@main/tafsir/en-al-jalalayn/1/1.json`
+  );
+  const tafsirresponse = await tafsir.json();
+  tafasir.push(tafsirresponse);
+  // }
+  return [quranresponse, tafasir];
 }
 // Wrap in async IIFE to use await at top level
-(() => __awaiter(void 0, void 0, void 0, function* () {
-    const [quran, tafsir] = yield aynsfunction();
-    console.log(quran);
-    console.log(tafsir);
-}))();
+(async () => {
+  const [quran, tafsir] = await aynsfunction();
+  console.log(quran);
+  console.log(tafsir);
+})();
 //# sourceMappingURL=chbeb.js.map
--- src/chbeb.ts
+++ src/chbeb.ts
@@ -1,9 +1,22 @@
 async function aynsfunction() {
   let tafasir: Array<string> = [];
+  // do {
+  //   const numberayah = Math.floor(Math.random() * 286 + 1);
+  //   const numbersurah = Math.floor(Math.random() * 114 + 1);
+  // } while ((numberayah) => {});
+
   const quran = await fetch(
     "https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/ara-quranwarsh/1/1.json"
   );
   const quranresponse = await quran.json();
+  const englishquran = await fetch(
+    "https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/eng-aliquliqarai1/1/1.json"
+  );
+  const englishresponse = await englishquran.json();
+  const frenshquran = await fetch(
+    "https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/fra-rashidmaash/1/1.json"
+  );
+  const frenshresponse = frenshquran.json();
   // for (let i = 0; i < 5; i++) {
   const tafsir = await fetch(
     `https://cdn.jsdelivr.net/gh/spa5k/tafsir_api@main/tafsir/en-al-jalalayn/1/1.json`
@@ -12,11 +25,30 @@ async function aynsfunction() {
   tafasir.push(tafsirresponse);
   // }
 
-  return [quranresponse, tafasir];
+  return [quranresponse, tafasir, englishresponse, frenshresponse];
 }
 // Wrap in async IIFE to use await at top level
-(async () => {
-  const [quran, tafsir] = await aynsfunction();
-  console.log(quran);
-  console.log(tafsir);
-})();
+const ayah = document.querySelector("#AYAH");
+const translate = document.querySelector("#ayahtranslate");
+const tfsir = document.querySelector("#tfsir");
+const number = document.querySelector("#number");
+const select = document.querySelector("#lang");
+const generate = document.querySelector("#Generate");
+
+try {
+  (async () => {
+    const [quran, tafsir, en, fr] = await aynsfunction();
+    console.log(quran);
+    console.log(tafsir);
+    console.log(en);
+    console.log(fr);
+  })();
+} catch (error) {
+  (async () => {
+    const [quran, tafsir, en, fr] = await aynsfunction();
+    console.log(quran);
+    console.log(tafsir);
+    console.log(en);
+    console.log(fr);
+  })();
+}
